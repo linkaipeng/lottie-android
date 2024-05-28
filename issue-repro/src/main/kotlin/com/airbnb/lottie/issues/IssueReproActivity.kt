8f90ece0e58b15584572ee1879427ebc81706127
linkaipeng <linkaipeng@cvte.com>
2024-05-28 17:20:09 +0800

    add issue demo
    add issue demo

--- issue-repro/src/main/kotlin/com/airbnb/lottie/issues/IssueReproActivity.kt
+++ issue-repro/src/main/kotlin/com/airbnb/lottie/issues/IssueReproActivity.kt
@@ -1,6 +1,7 @@
 package com.airbnb.lottie.issues
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.issues.databinding.IssueReproActivityBinding
 
@@ -10,5 +11,18 @@ class IssueReproActivity : AppCompatActivity() {
         val binding = IssueReproActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         // Reproduce any issues here.
+
+        binding.animationView.addLottieOnCompositionLoadedListener {
+            Log.i("IssueReproActivity", "Lottie composition loaded")
+            binding.animationView.playAnimation()
+        }
+
+        binding.cancelButton.setOnClickListener {
+            binding.animationView.cancelAnimation()
+        }
+
+        binding.loadButton.setOnClickListener {
+            binding.animationView.setAnimation(R.raw.heart)
+        }
     }
 }
